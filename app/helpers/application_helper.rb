b92247065dee16b995e2cb4b35c50cf046b79b9b
Mark Hyams <markhyams@gmail.com>
2020-04-27 10:30:59 -0600

    popper.js, modals all work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def list_tags(ticket_obj)
     ticket_obj.tags.map { |tag| tag.name }.join(', ')
   end
+
+  def path_to_obj(obj)
+    '/' + obj.class.table_name + '/' + obj.id.to_s
+  end
 end
+
