4563ea27676f23858cf10651db4641ce791a9fb1
Mark Hyams <markhyams@gmail.com>
2020-04-29 20:19:20 -0600

    step 19 done

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,7 +4,7 @@ class Tag < ApplicationRecord
 
   def self.count_all
     sql = <<-SQL
-      SELECT q.count, tags.id, q.name FROM tags INNER JOIN (SELECT COUNT(tags_tickets.tag_id) AS count, tags.name AS name FROM tags_tickets RIGHT OUTER JOIN tags ON tags.id = tags_tickets.tag_id GROUP BY tags.name) q ON tags.name = q.name;
+      SELECT COUNT(tags_tickets.tag_id) AS count, tags.name, tags.id AS id FROM tags_tickets RIGHT OUTER JOIN tags ON tags.id = tags_tickets.tag_id GROUP BY tags.name, tags.id ORDER BY tags.name;
     SQL
 
     result = ActiveRecord::Base.connection.execute(sql)
